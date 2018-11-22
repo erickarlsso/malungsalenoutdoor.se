4d1e761d484bc6a2bec2730544b5d6e983f3a9b0
Eric Karlsson <eric-karlsson@outlook.com>
2018-11-22 08:05:07 +0100

    Uppdatering

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import Swiper from 'swiper';
+import fitText from './libs/fittext';
+
 document.addEventListener('DOMContentLoaded', () => {
 
   /* listen for clicks on elements with data-toggle="navigation", then show navigation */
@@ -7,8 +10,24 @@ document.addEventListener('DOMContentLoaded', () => {
     dataToggleNavigation.forEach( el => {
       el.addEventListener('click', () => {
         document.body.classList.toggle('navigation-is-visible');
+        el.classList.toggle('c-hamburger--is-active');
       });
     });
   }
 
 });
+
+var featureTodo = new Swiper ('.c-swiper--home', {
+  /* modify classes */
+  slideActiveClass: 'c-swiper__slide--active',
+  virtualTranslate: true,
+  slideToClickedSlide: true,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+/* fittext */
+window.fitText( document.getElementById('js-aboutHeading',), 0.5 );

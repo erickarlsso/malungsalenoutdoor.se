4bfe38d2f3f812c3663e15b00ded4bc4be75f562
Eric Karlsson <eric-karlsson@outlook.com>
2018-11-23 15:59:15 +0100

    Update

--- docs/js/main.js
+++ docs/js/main.js
@@ -170,30 +170,54 @@ document.addEventListener('DOMContentLoaded', function () {
 
   /* listen for clicks on elements with data-toggle="navigation", then show navigation */
   var dataToggleNavigation = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="navigation"]'), 0);
+  var oSite = document.getElementById('js-site');
 
   if (dataToggleNavigation.length > 0) {
     dataToggleNavigation.forEach(function (el) {
       el.addEventListener('click', function () {
-        document.body.classList.toggle('navigation-is-visible');
+        oSite.classList.toggle('o-site--navigation-is-visible');
         el.classList.toggle('c-hamburger--is-active');
       });
     });
   }
+
+  /* listen for scroll more than Xpx, then if true add class to header */
+  var siteHeader = document.getElementById('js-header');
+  var scrollpos = window.scrollY;
+
+  window.addEventListener('scroll', function () {
+    scrollpos = window.scrollY;
+
+    if (scrollpos > 10) {
+      siteHeader.classList.add('c-header--sticky');
+    } else {
+      siteHeader.classList.remove('c-header--sticky');
+    }
+  });
+
+  var featureIntroSwiper = new __WEBPACK_IMPORTED_MODULE_0_swiper__["a" /* default */]('.c-feature--intro', {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.c-swiper__button--next',
+      prevEl: '.c-swiper__button--prev'
+    }
+  });
+
+  /* fittext */
+  // .c-feature--intro
+  window.fitText(document.getElementById('js-featureIntroOne'), 2.25);
+  window.fitText(document.getElementById('js-featureIntroTwo'), 0.85);
+  window.fitText(document.getElementById('js-featureIntroThree'), 3.95);
+
+  // .c-feature--todo
+  window.fitText(document.getElementById('js-featureTodoFour'), 0.525);
+  window.fitText(document.getElementById('js-featureTodoFive'), 1);
 });
-
-var featureTodo = new __WEBPACK_IMPORTED_MODULE_0_swiper__["a" /* default */]('#js-todoSwipes', {
-  /* modify classes */
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar'
-  }
-});
-
-/* fittext */
-window.fitText(document.getElementById('js-todoHeading'), 0.55);
-window.fitText(document.getElementById('js-todoSubHeading'), 1.39);
-window.fitText(document.getElementById('js-aboutHeading'), 0.5);
 
 /***/ }),
 /* 3 */
--- src/js/main.js
+++ src/js/main.js
@@ -5,28 +5,52 @@ document.addEventListener('DOMContentLoaded', () => {
 
   /* listen for clicks on elements with data-toggle="navigation", then show navigation */
   const dataToggleNavigation = Array.prototype.slice.call(document.querySelectorAll('[data-toggle="navigation"]'), 0);
+  const oSite = document.getElementById('js-site');
 
   if (dataToggleNavigation.length > 0) {
     dataToggleNavigation.forEach( el => {
       el.addEventListener('click', () => {
-        document.body.classList.toggle('navigation-is-visible');
+        oSite.classList.toggle('o-site--navigation-is-visible');
         el.classList.toggle('c-hamburger--is-active');
       });
     });
   }
 
+  /* listen for scroll more than Xpx, then if true add class to header */
+  const siteHeader = document.getElementById('js-header');
+  let scrollpos = window.scrollY;
+
+  window.addEventListener('scroll', function(){ 
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 10){
+      siteHeader.classList.add('c-header--sticky');
+    } else {
+      siteHeader.classList.remove('c-header--sticky');
+    }
+  });
+
+  const featureIntroSwiper = new Swiper ('.c-feature--intro', {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.c-swiper__button--next',
+      prevEl: '.c-swiper__button--prev',
+    },
+  });
+
+  /* fittext */
+  // .c-feature--intro
+  window.fitText( document.getElementById('js-featureIntroOne',), 2.25 );
+  window.fitText( document.getElementById('js-featureIntroTwo',), 0.85 );
+  window.fitText( document.getElementById('js-featureIntroThree',), 3.95 );
+
+  // .c-feature--todo
+  window.fitText( document.getElementById('js-featureTodoFour',), 0.525 );
+  window.fitText( document.getElementById('js-featureTodoFive',), 1 );
+
 });
-
-var featureTodo = new Swiper ('#js-todoSwipes', {
-  /* modify classes */
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-})
-
-/* fittext */
-window.fitText( document.getElementById('js-todoHeading',), 0.55 );
-window.fitText( document.getElementById('js-todoSubHeading',), 1.39 );
-window.fitText( document.getElementById('js-aboutHeading',), 0.5 );
